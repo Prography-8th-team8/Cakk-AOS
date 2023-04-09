3e32c55fdafd0c84b6883d0f34d69410b79c7db1
lee989898 <wnsrud1352@gmail.com>
2023-04-10 02:30:00 +0900

    [chore] #1 build.gradle.kts(project) 추가

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    dependencies {
+        classpath(libs.plugin.hilt)
+        classpath(libs.plugin.androidGradle)
+        classpath(libs.plugin.kotlin)
+    }
+}
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+}
+
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
+}
