3e40a46c7541f17e6bc81af1862a6a97b7810273
lee989898 <wnsrud1352@gmail.com>
2023-04-13 20:17:54 +0900

    [chore] #1 editorconfig 파일 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ ktlint {
     coloredOutput.set(true)
     verbose.set(true)
     outputToConsole.set(true)
-    disabledRules.set(setOf("max-line-length", "no-wildcard-imports", "import-ordering"))
     reporters {
         reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
         reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.CHECKSTYLE)
