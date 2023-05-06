753be8bf0b7a2ebaec74c164ac7c628af0252f94
ows3090 <ows3090@naver.com>
2023-05-06 23:02:34 +0900

    [feat] #10 Onboarding 화면에 필요한 지역 정보 클래스 정의

---
+++ feature/onboarding/src/main/java/org/prography/onboarding/RegionItem.kt
@@ -0,0 +1,10 @@
+package org.prography.onboarding
+
+import androidx.compose.ui.graphics.Color
+
+data class RegionItem(
+    val id: Int,
+    val region: String,
+    val count: Int,
+    val color: Color,
+)
