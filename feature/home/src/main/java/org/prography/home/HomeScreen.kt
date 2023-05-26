5774b24aca7ae4917c4722e47f776c789246c1a4
lee989898 <wnsrud1352@gmail.com>
2023-05-26 19:33:37 +0900

    [feat] #17 HomeScreen 마커 찍기 구현

--- feature/home/src/main/java/org/prography/home/HomeScreen.kt
+++ feature/home/src/main/java/org/prography/home/HomeScreen.kt
@@ -1,17 +1,45 @@
 package org.prography.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.Marker
+import com.naver.maps.map.compose.MarkerState
 import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.overlay.OverlayImage
+import org.prography.designsystem.R
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun HomeScreen(
     navHostController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier,
+    homeViewModel: HomeViewModel = hiltViewModel(),
 ) {
-    NaverMap()
+    val storeList = homeViewModel.stores.collectAsState().value
+
+    LaunchedEffect(true) {
+        homeViewModel.fetchStoreList()
+    }
+
+    Box {
+        NaverMap(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            storeList?.forEach {
+                Marker(
+                    // 나중에 it.latitude, it.longitude로 변경
+                    state = MarkerState(position = LatLng(37.532600, 127.024612)),
+                    icon = OverlayImage.fromResource(R.drawable.ic_marker),
+                )
+            }
+        }
+    }
 }
