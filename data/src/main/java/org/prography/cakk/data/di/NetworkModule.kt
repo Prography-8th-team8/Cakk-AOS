c5299c54a370af7123d9302f925af0f75b949ce6
lee989898 <wnsrud1352@gmail.com>
2023-05-26 19:30:08 +0900

    [chore] #17 NetworkModule에 getHttpClient 추가

--- data/src/main/java/org/prography/cakk/data/di/NetworkModule.kt
+++ data/src/main/java/org/prography/cakk/data/di/NetworkModule.kt
@@ -1,3 +1,24 @@
 package org.prography.cakk.data.di
 
-object NetworkModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import org.prography.cakk.data.CakkHttpClient
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun getHttpClient(httpClient: CakkHttpClient): HttpClient = httpClient.getHttpClient()
+}
