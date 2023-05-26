4d5d19c5f5ef7e0d3ea2a014eac657c0281e8833
lee989898 <wnsrud1352@gmail.com>
2023-05-26 19:30:42 +0900

    [chore] #17 RepositoryModule에 bindStoreRepository 추가

--- data/src/main/java/org/prography/cakk/data/di/RepositoryModule.kt
+++ data/src/main/java/org/prography/cakk/data/di/RepositoryModule.kt
@@ -1,3 +1,20 @@
 package org.prography.cakk.data.di
 
-interface RepositoryModule
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.prography.cakk.data.repository.StoreRepository
+import org.prography.cakk.data.repository.StoreRepositoryImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule {
+
+    @Binds
+    @Singleton
+    fun bindStoreRepository(
+        storeRepositoryImpl: StoreRepositoryImpl,
+    ): StoreRepository
+}
