74274b1fd0b8fb104744c5d3cb84bfaad6e324f0
lee989898 <wnsrud1352@gmail.com>
2023-05-27 22:40:31 +0900

    [chore] #17 stateIn 함수로 변경

--- feature/home/src/main/java/org/prography/home/HomeViewModel.kt
+++ feature/home/src/main/java/org/prography/home/HomeViewModel.kt
@@ -3,31 +3,20 @@ package org.prography.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 import org.prography.cakk.data.api.model.request.StoreListRequest
-import org.prography.cakk.data.api.model.response.StoreListResponse
 import org.prography.cakk.data.repository.StoreRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val storeRepository: StoreRepository,
+    storeRepository: StoreRepository,
 ) : ViewModel() {
 
-    private val _stores = MutableStateFlow<List<StoreListResponse>?>(null)
-    val stores = _stores.asStateFlow()
-
-    fun fetchStoreList(districtType: String = "JONGNO", page: Int = 1) {
-        viewModelScope.launch {
-            runCatching {
-                storeRepository.fetchStoreList(StoreListRequest(districtType, page))
-            }.onSuccess {
-                _stores.value = it
-            }.onFailure {
-                it.message
-            }
-        }
-    }
+    val stores = storeRepository.fetchStoreList(StoreListRequest("JONGNO", 1)).stateIn(
+        initialValue = listOf(),
+        started = SharingStarted.WhileSubscribed(),
+        scope = viewModelScope
+    )
 }
