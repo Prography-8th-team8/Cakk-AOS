e318d0e3d95f397bbd6ca72f8e53866ec4263179
lee989898 <wnsrud1352@gmail.com>
2023-05-27 22:39:46 +0900

    [chore] #17 네트워크 모듈에 필요한 의존성, 플로그인 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,6 +1,9 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
@@ -13,10 +16,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.bundles.dagger)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.bundles.ktor)
+    implementation(libs.material)
+    implementation(libs.junit)
+    implementation(libs.timber)
+    kapt(libs.bundles.compiler)
 }
