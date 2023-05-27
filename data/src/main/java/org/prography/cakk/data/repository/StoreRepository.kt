7bb12d2556a4d8bbe5d5e7394ba6a96b04afeb8a
lee989898 <wnsrud1352@gmail.com>
2023-05-27 22:37:33 +0900

    [chore] #17 suspend 키워드 제거 및 flow로 반환값 변경

--- data/src/main/java/org/prography/cakk/data/repository/StoreRepository.kt
+++ data/src/main/java/org/prography/cakk/data/repository/StoreRepository.kt
@@ -1,9 +1,10 @@
 package org.prography.cakk.data.repository
 
+import kotlinx.coroutines.flow.Flow
 import org.prography.cakk.data.api.model.request.StoreListRequest
 import org.prography.cakk.data.api.model.response.StoreListResponse
 
 interface StoreRepository {
 
-    suspend fun fetchStoreList(storeListRequest: StoreListRequest): List<StoreListResponse>
+    fun fetchStoreList(storeListRequest: StoreListRequest): Flow<List<StoreListResponse>>
 }
--- data/src/main/java/org/prography/cakk/data/repository/StoreRepositoryImpl.kt
+++ data/src/main/java/org/prography/cakk/data/repository/StoreRepositoryImpl.kt
@@ -2,6 +2,8 @@ package org.prography.cakk.data.repository
 
 import io.ktor.client.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.prography.cakk.data.BuildConfig
 import org.prography.cakk.data.api.model.request.StoreListRequest
 import org.prography.cakk.data.api.model.response.StoreListResponse
@@ -11,11 +13,13 @@ class StoreRepositoryImpl @Inject constructor(
     private val httpClient: HttpClient,
 ) : StoreRepository {
 
-    override suspend fun fetchStoreList(storeListRequest: StoreListRequest): List<StoreListResponse> {
-        return httpClient.get {
-            url("${BuildConfig.CAKK_BASE_URL}api/store/list")
-            parameter("district", storeListRequest.district)
-            parameter("page", storeListRequest.page)
+    override fun fetchStoreList(storeListRequest: StoreListRequest): Flow<List<StoreListResponse>> {
+        return flow {
+            httpClient.get {
+                url("${BuildConfig.CAKK_BASE_URL}api/store/list")
+                parameter("district", storeListRequest.district)
+                parameter("page", storeListRequest.page)
+            }
         }
     }
 }
