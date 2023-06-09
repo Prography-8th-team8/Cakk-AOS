21de30daa64c29eaf7b359a2f22418b0f7059097
ows3090 <ows3090@naver.com>
2023-06-09 13:34:01 +0900

    [feat] #20 케이크샵 상세 정보 조회 인터페이스 정의 및 구현

--- data/src/main/java/org/prography/cakk/data/repository/StoreRepository.kt
+++ data/src/main/java/org/prography/cakk/data/repository/StoreRepository.kt
@@ -2,9 +2,12 @@ package org.prography.cakk.data.repository
 
 import kotlinx.coroutines.flow.Flow
 import org.prography.cakk.data.api.model.request.StoreListRequest
+import org.prography.cakk.data.api.model.response.StoreDetailResponse
 import org.prography.cakk.data.api.model.response.StoreListResponse
 
 interface StoreRepository {
 
     fun fetchStoreList(storeListRequest: StoreListRequest): Flow<List<StoreListResponse>>
+
+    fun fetchDetailStore(storeId: Int): Flow<StoreDetailResponse>
 }
--- data/src/main/java/org/prography/cakk/data/repository/StoreRepositoryImpl.kt
+++ data/src/main/java/org/prography/cakk/data/repository/StoreRepositoryImpl.kt
@@ -5,8 +5,10 @@ import io.ktor.client.request.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.prography.cakk.data.api.model.request.StoreListRequest
+import org.prography.cakk.data.api.model.response.StoreDetailResponse
 import org.prography.cakk.data.api.model.response.StoreListResponse
 import org.prography.network.CakkService.BASE_URL
+import org.prography.network.CakkService.Endpoint.STORE_DETAIL
 import org.prography.network.CakkService.Endpoint.STORE_LIST
 import org.prography.network.CakkService.Parameter.DISTRICT
 import org.prography.network.CakkService.Parameter.PAGE
@@ -26,5 +28,13 @@ class StoreRepositoryImpl @Inject constructor(
                 }
             )
         }
+    }
+
+    override fun fetchDetailStore(storeId: Int): Flow<StoreDetailResponse> = flow {
+        emit(
+            httpClient.get(path = "$storeId") {
+                url("$BASE_URL$STORE_DETAIL")
+            }
+        )
     }
 }
