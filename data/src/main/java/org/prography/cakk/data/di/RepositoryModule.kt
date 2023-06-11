ea166717628f32cb54382c0cc96cc23ed050b733
ows3090 <ows3090@naver.com>
2023-06-11 16:18:11 +0900

    [feat] #29 온보딩 지역 정보 조회 Repository DI 적용

--- data/src/main/java/org/prography/cakk/data/di/RepositoryModule.kt
+++ data/src/main/java/org/prography/cakk/data/di/RepositoryModule.kt
@@ -4,8 +4,10 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import org.prography.cakk.data.repository.StoreRepository
-import org.prography.cakk.data.repository.StoreRepositoryImpl
+import org.prography.cakk.data.repository.district.DistrictRepository
+import org.prography.cakk.data.repository.district.DistrictRepositoryImpl
+import org.prography.cakk.data.repository.store.StoreRepository
+import org.prography.cakk.data.repository.store.StoreRepositoryImpl
 import javax.inject.Singleton
 
 @Module
@@ -17,4 +19,10 @@ interface RepositoryModule {
     fun bindStoreRepository(
         storeRepositoryImpl: StoreRepositoryImpl,
     ): StoreRepository
+
+    @Binds
+    @Singleton
+    fun bindDistrictRepository(
+        districtRepositoryImpl: DistrictRepositoryImpl,
+    ): DistrictRepository
 }
