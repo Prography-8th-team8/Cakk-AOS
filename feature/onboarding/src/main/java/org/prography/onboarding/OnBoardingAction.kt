87afdb2828b1b7d38aab4afd50fbfdd2d3b464fb
ows3090 <ows3090@naver.com>
2023-06-11 17:27:20 +0900

    [feat] #29 온보딩 화면에서 필요한 Action 추가

--- feature/onboarding/src/main/java/org/prography/onboarding/OnBoardingAction.kt
+++ feature/onboarding/src/main/java/org/prography/onboarding/OnBoardingAction.kt
@@ -2,4 +2,11 @@ package org.prography.onboarding
 
 import org.prography.base.BaseAction
 
-sealed class OnBoardingAction : BaseAction
+sealed class OnBoardingAction : BaseAction {
+    object Loading : OnBoardingAction()
+    object LoadDistrictList : OnBoardingAction()
+
+    data class LoadedDistrictList(
+        val districts: List<DistrictModel>,
+    ) : OnBoardingAction()
+}
