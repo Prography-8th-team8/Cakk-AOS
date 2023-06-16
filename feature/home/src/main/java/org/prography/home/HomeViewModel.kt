fd23133e47a387e543b51f5fc9729a821cd2c85c
ows3090 <ows3090@naver.com>
2023-06-16 15:13:08 +0900

    [feat] #31 HomeScreen의 비즈니스 로직을 담당하는 ViewModel 구현, MVI 준수

--- feature/home/src/main/java/org/prography/home/HomeViewModel.kt
+++ feature/home/src/main/java/org/prography/home/HomeViewModel.kt
@@ -1,22 +1,39 @@
 package org.prography.home
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
+import org.prography.base.BaseViewModel
 import org.prography.cakk.data.api.model.request.StoreListRequest
 import org.prography.cakk.data.repository.store.StoreRepository
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    storeRepository: StoreRepository,
-) : ViewModel() {
+    private val storeRepository: StoreRepository,
+) : BaseViewModel<HomeUiAction, HomeUiState, HomeUiSideEffect>(
+    initialState = HomeUiState()
+) {
+    override fun reduceState(currentState: HomeUiState, action: HomeUiAction): HomeUiState = when (action) {
+        HomeUiAction.Loading -> currentState
+        is HomeUiAction.LoadStoreList -> {
+            fetchStoreList(action.districts)
+            currentState
+        }
+        is HomeUiAction.LoadedStoreList -> {
+            currentState.copy(storeModels = currentState.storeModels + action.storeModels)
+        }
+    }
 
-    val stores = storeRepository.fetchStoreList(StoreListRequest("JONGNO", 1)).stateIn(
-        initialValue = listOf(),
-        started = SharingStarted.WhileSubscribed(),
-        scope = viewModelScope
-    )
+    @OptIn(FlowPreview::class)
+    private fun fetchStoreList(districts: List<String>) {
+        Timber.i("fetchStoreList ${districts.first()}")
+        districts.asFlow()
+            .flatMapMerge { storeRepository.fetchStoreList(StoreListRequest(it, 1)) }
+            .onStart { sendAction(HomeUiAction.Loading) }
+            .onEach { sendAction(HomeUiAction.LoadedStoreList(it.toModel())) }
+            .launchIn(viewModelScope)
+    }
 }
