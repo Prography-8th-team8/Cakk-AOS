4fc69070edea87e4d3c5d4bf68878cbbd5e62d82
ows3090 <ows3090@naver.com>
2023-06-17 17:47:42 +0900

    [feat] #31 홈 화면에 필요한 상태에 마지막 바텀 시트 상태값 추가

--- feature/home/src/main/java/org/prography/home/HomeUiState.kt
+++ feature/home/src/main/java/org/prography/home/HomeUiState.kt
@@ -1,7 +1,31 @@
 package org.prography.home
 
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import org.prography.base.BaseState
 
 data class HomeUiState(
     val storeModels: List<StoreModel> = listOf(),
+    val lastExpandedType: ExpandedType = ExpandedType.HALF,
 ) : BaseState
+
+enum class ExpandedType {
+    HALF, FULL, COLLAPSED, MOVING;
+
+    fun getByScreenHeight(type: ExpandedType, screenHeight: Int, statusBarHeight: Int, offsetY: Float): Dp {
+        return when (type) {
+            FULL -> {
+                (screenHeight - statusBarHeight).dp
+            }
+            HALF -> {
+                ((screenHeight / 2.5).toInt()).dp
+            }
+            COLLAPSED -> {
+                53.dp
+            }
+            MOVING -> {
+                offsetY.dp
+            }
+        }
+    }
+}
