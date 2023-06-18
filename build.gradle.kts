6f359edfe4db96b3c1bf73c7b86296d0c030ea25
ows3090 <ows3090@naver.com>
2023-06-19 00:48:46 +0900

    [feat] #34 domain 모듈(kotlin-library) 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.secrets.gradle) apply false
     alias(libs.plugins.serialization) apply false
     alias(libs.plugins.dagger.hilt) apply false
