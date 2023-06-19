7f8bdba9aa2bd103bce6daa3dfb7d64bb66a659c
ows3090 <ows3090@naver.com>
2023-06-20 02:59:19 +0900

    [feat] #34 StoreRepository를 통해 바로 요청하는 것이 아닌 DataSource 를 한단계 거치도록 구현

--- data/src/main/java/org/prography/cakk/data/repository/StoreRepositoryImpl.kt
+++ data/src/main/java/org/prography/cakk/data/repository/StoreRepositoryImpl.kt
@@ -1,43 +1,24 @@
 package org.prography.cakk.data.repository
 
-import io.ktor.client.*
-import io.ktor.client.request.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import org.prography.cakk.data.api.dto.response.StoreDetailResponse
-import org.prography.cakk.data.api.dto.response.StoreListResponse
+import kotlinx.coroutines.flow.map
+import org.prography.cakk.data.datasource.StoreRemoteSource
 import org.prography.domain.model.store.StoreDetailModel
 import org.prography.domain.model.store.StoreModel
 import org.prography.domain.repository.StoreRepository
-import org.prography.network.CakkService.BASE_URL
-import org.prography.network.CakkService.Endpoint.STORE_DETAIL
-import org.prography.network.CakkService.Endpoint.STORE_LIST
-import org.prography.network.CakkService.Parameter.DISTRICT
-import org.prography.network.CakkService.Parameter.PAGE
+import org.prography.utility.mapper.toModel
 import javax.inject.Inject
 
 class StoreRepositoryImpl @Inject constructor(
-    private val httpClient: HttpClient,
+    private val storeRemoteSource: StoreRemoteSource
 ) : StoreRepository {
 
     override fun fetchStoreList(
         district: String,
         page: Int
-    ): Flow<List<StoreModel>> = flow {
-        emit(
-            httpClient.get<List<StoreListResponse>> {
-                url("$BASE_URL$STORE_LIST")
-                parameter(DISTRICT, district)
-                parameter(PAGE, page)
-            }.map { it.toModel() }
-        )
-    }
+    ): Flow<List<StoreModel>> =
+        storeRemoteSource.fetchStoreList(district, page).map { it.toModel() }
 
-    override fun fetchDetailStore(storeId: Int): Flow<StoreDetailModel> = flow {
-        emit(
-            httpClient.get<StoreDetailResponse> {
-                url("$BASE_URL$STORE_DETAIL/$storeId")
-            }.toModel()
-        )
-    }
+    override fun fetchDetailStore(storeId: Int): Flow<StoreDetailModel> =
+        storeRemoteSource.fetchDetailStore(storeId).map { it.toModel() }
 }
