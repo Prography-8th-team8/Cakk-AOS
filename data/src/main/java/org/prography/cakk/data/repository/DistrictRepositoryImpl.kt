17b55088d1d66c49f9100f45c85e475a367cc83d
ows3090 <ows3090@naver.com>
2023-06-20 02:59:13 +0900

    [feat] #34 DistrictRepository를 통해 바로 요청하는 것이 아닌 DataSource 를 한단계 거치도록 구현

--- data/src/main/java/org/prography/cakk/data/repository/DistrictRepositoryImpl.kt
+++ data/src/main/java/org/prography/cakk/data/repository/DistrictRepositoryImpl.kt
@@ -1,24 +1,16 @@
 package org.prography.cakk.data.repository
 
-import io.ktor.client.*
-import io.ktor.client.request.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import org.prography.cakk.data.api.model.response.DistrictResponse
+import kotlinx.coroutines.flow.map
+import org.prography.cakk.data.datasource.DistrictRemoteSource
 import org.prography.domain.model.district.DistrictModel
 import org.prography.domain.repository.DistrictRepository
-import org.prography.network.CakkService.BASE_URL
-import org.prography.network.CakkService.Endpoint.DISTRICT_LIST
+import org.prography.utility.mapper.toModel
 import javax.inject.Inject
 
 class DistrictRepositoryImpl @Inject constructor(
-    private val httpClient: HttpClient,
+    private val districtRemoteSource: DistrictRemoteSource
 ) : DistrictRepository {
-    override fun fetchDistrictList(): Flow<List<DistrictModel>> = flow {
-        emit(
-            httpClient.get<List<DistrictResponse>> {
-                url("$BASE_URL$DISTRICT_LIST")
-            }.map { it.toModel() }
-        )
-    }
+    override fun fetchDistrictList(): Flow<List<DistrictModel>> =
+        districtRemoteSource.fetchDistrictList().map { it.toModel() }
 }
