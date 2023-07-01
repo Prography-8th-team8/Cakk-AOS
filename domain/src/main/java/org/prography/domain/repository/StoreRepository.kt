3a39187a0460c06ba36305aa963066356d186494
ows3090 <ows3090@naver.com>
2023-07-02 00:31:46 +0900

    [feat] #36 케이크샵 블로그 리뷰 API 연동

--- domain/src/main/java/org/prography/domain/repository/StoreRepository.kt
+++ domain/src/main/java/org/prography/domain/repository/StoreRepository.kt
@@ -1,15 +1,14 @@
 package org.prography.domain.repository
 
 import kotlinx.coroutines.flow.Flow
+import org.prography.domain.model.store.StoreBlogModel
 import org.prography.domain.model.store.StoreDetailModel
 import org.prography.domain.model.store.StoreModel
 
 interface StoreRepository {
-
-    fun fetchStoreList(
-        district: String,
-        page: Int
-    ): Flow<List<StoreModel>>
+    fun fetchStoreList(district: String, page: Int): Flow<List<StoreModel>>
 
     fun fetchDetailStore(storeId: Int): Flow<StoreDetailModel>
+
+    fun fetchStoreBlog(storeId: Int): Flow<StoreBlogModel>
 }
