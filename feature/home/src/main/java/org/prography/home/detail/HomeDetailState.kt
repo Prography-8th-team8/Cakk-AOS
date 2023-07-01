7d5686aee2197cd8d4081865e3058954914d18fd
ows3090 <ows3090@naver.com>
2023-07-02 00:35:13 +0900

    [feat] #36 케이크샵 상세화면의 State에 블로그 리뷰 정보 추가

--- feature/home/src/main/java/org/prography/home/detail/HomeDetailState.kt
+++ feature/home/src/main/java/org/prography/home/detail/HomeDetailState.kt
@@ -1,8 +1,10 @@
 package org.prography.home.detail
 
 import org.prography.base.BaseState
+import org.prography.domain.model.store.BlogPostModel
 import org.prography.domain.model.store.StoreDetailModel
 
 data class HomeDetailState(
     val storeDetailModel: StoreDetailModel = StoreDetailModel(),
+    val blogPosts: List<BlogPostModel> = listOf()
 ) : BaseState
