1012fe858f75ddd58c98290e0b229b4866e2be0e
ows3090 <ows3090@naver.com>
2023-07-02 00:33:31 +0900

    [feat] #36 케이크샵 상세화면의 블로그 API 요청 이벤트 추가

--- feature/home/src/main/java/org/prography/home/detail/HomeDetailAction.kt
+++ feature/home/src/main/java/org/prography/home/detail/HomeDetailAction.kt
@@ -1,10 +1,13 @@
 package org.prography.home.detail
 
 import org.prography.base.BaseAction
+import org.prography.domain.model.store.BlogPostModel
 import org.prography.domain.model.store.StoreDetailModel
 
 sealed class HomeDetailAction : BaseAction {
     object Loading : HomeDetailAction()
     data class LoadDetailInfo(val id: Int) : HomeDetailAction()
     data class LoadedDetailInfo(val storeDetailModel: StoreDetailModel) : HomeDetailAction()
+    data class LoadBlogInfos(val id: Int) : HomeDetailAction()
+    data class LoadedBlogInfos(val blogPosts: List<BlogPostModel>) : HomeDetailAction()
 }
