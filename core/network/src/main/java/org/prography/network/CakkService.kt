05634091347f744f23691c3b25d84f9483095d96
ows3090 <ows3090@naver.com>
2023-07-05 14:53:19 +0900

    [feat] #41 케이크샵 지역 재검색 API Endpoint, Parameter 추가

--- core/network/src/main/java/org/prography/network/CakkService.kt
+++ core/network/src/main/java/org/prography/network/CakkService.kt
@@ -8,10 +8,16 @@ object CakkService {
         const val STORE_DETAIL = "api/store"
         const val DISTRICT_LIST = "api/store/district/count"
         const val STORE_BLOG = "blog"
+        const val STORE_RELOAD = "api/store/reload"
     }
 
     object Parameter {
         const val DISTRICT = "district"
         const val PAGE = "page"
+        const val STORE_TYPES = "storeTypes"
+        const val SOUTH_WEST_LATITUDE = "southwestLatitude"
+        const val SOUTH_WEST_LONGITUDE = "southwestLongitude"
+        const val NORTH_EAST_LATITUDE = "northeastLatitude"
+        const val NORTH_EAST_LONGITUDE = "northeastLongitude"
     }
 }
