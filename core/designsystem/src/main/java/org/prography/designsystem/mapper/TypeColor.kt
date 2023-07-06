54f845699f94c4e5c85954e17eadce8ad7867857
lee989898 <wnsrud1352@gmail.com>
2023-07-06 21:41:31 +0900

    [feat] #39 StoreType 배경색 가져오는 확장함수 구현

--- core/designsystem/src/main/java/org/prography/designsystem/mapper/TypeColor.kt
+++ core/designsystem/src/main/java/org/prography/designsystem/mapper/TypeColor.kt
@@ -1,6 +1,6 @@
 package org.prography.designsystem.mapper
 
-import org.prography.cakk.data.api.model.enums.DistrictType
+import org.prography.domain.model.enums.DistrictType
 import org.prography.domain.model.enums.StoreType
 import org.prography.designsystem.ui.theme.*
 
@@ -14,8 +14,22 @@ fun StoreType.toColor() = when (this) {
     StoreType.FIGURE -> Yankees_Blue
     StoreType.TIARA -> Cerise
     StoreType.LUXURY -> Cerise // 추후 색상 변경
-    StoreType.SOLID -> Cerise // 추후 색상 변경
-    StoreType.ETC -> Cerise // 추후 색상 변경
+    StoreType.SOLID -> DeepMagenta
+    StoreType.ETC -> Raisin_Black
+}
+
+fun StoreType.toBackgroundColor(isSelected: Boolean) = when (this) {
+    StoreType.CHARACTER -> if (isSelected) Light_Deep_Pink.copy(alpha = 0.2f) else White
+    StoreType.LETTERING -> if (isSelected) Palatinate_Blue.copy(alpha = 0.2f) else White
+    StoreType.RICE -> if (isSelected) Medium_Slate_Blue.copy(alpha = 0.2f) else White
+    StoreType.MEALBOX -> if (isSelected) Mustard_Yellow.copy(alpha = 0.2f) else White
+    StoreType.FLOWER -> if (isSelected) Maximum_Red.copy(alpha = 0.12f) else White
+    StoreType.PHOTO -> if (isSelected) Yankees_Blue.copy(alpha = 0.2f) else White
+    StoreType.FIGURE -> if (isSelected) Metallic_Sunburst.copy(alpha = 0.2f) else White
+    StoreType.TIARA -> if (isSelected) Palatinate_Blue.copy(alpha = 0.1f) else White
+    StoreType.LUXURY -> White // 추후 색상 변경
+    StoreType.SOLID -> if (isSelected) Cerise.copy(alpha = 0.2f) else White
+    StoreType.ETC -> if (isSelected) Raisin_Black.copy(alpha = 0.2f) else White
 }
 
 fun DistrictType.toColor() = when (this.groupId) {
