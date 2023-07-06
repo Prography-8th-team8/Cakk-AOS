80398ad017a541bee445f3bd9acf5ac9dfe986ac
lee989898 <wnsrud1352@gmail.com>
2023-07-06 21:39:46 +0900

    [chore] #39 StoreType에 럭셔리, 고체, 기타 추가

--- domain/src/main/java/org/prography/domain/model/enums/StoreType.kt
+++ domain/src/main/java/org/prography/domain/model/enums/StoreType.kt
@@ -2,14 +2,14 @@ package org.prography.domain.model.enums
 
 enum class StoreType(val tag: String) {
     CHARACTER("캐릭터"),
+    SOLID("입체 케이크"),
+    PHOTO("포토"),
+    MEALBOX("도시락"),
+    FIGURE("피규어"),
+    FLOWER("플라워"),
     LETTERING("레터링"),
     RICE("떡케이크"),
-    MEALBOX("도시락"),
-    FLOWER("플라워"),
-    PHOTO("포토"),
-    FIGURE("피규어"),
     TIARA("티아라"),
     LUXURY("럭셔리"), // 추후 태그명 변경
-    SOLID("고체"), // 추후 태그명 변경
-    ETC("기타") // 추후 태그명 변경
+    ETC("기타")
 }
