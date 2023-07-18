2dc24b89c981edd01cd08b507c0fab692ea58675
ows3090 <ows3090@naver.com>
2023-07-18 15:31:26 +0900

    [build] #50 케이크샵 상세화면 이미지 로딩을 위한 Coil 의존성 추가

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.androidx.compose.navigation)
     implementation(libs.material)
     implementation(libs.timber)
+    implementation(libs.coil.compose)
     implementation(libs.dagger.hilt.android)
     implementation(libs.bundles.androidx.compose)
     implementation(libs.naver.map)
