ffcc99a934b4028f7bb88681504cea7c23da4114
ows3090 <ows3090@naver.com>
2023-07-18 16:32:45 +0900

    [res] #50 홈 상세화면 Header 개발 완료

--- feature/home/src/main/java/org/prography/home/detail/HomeDetailScreen.kt
+++ feature/home/src/main/java/org/prography/home/detail/HomeDetailScreen.kt
@@ -10,15 +10,15 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -29,122 +29,296 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import org.prography.designsystem.R
+import org.prography.designsystem.component.CakkAppbarWithBack
 import org.prography.designsystem.mapper.toColor
 import org.prography.designsystem.ui.theme.*
 import org.prography.domain.model.enums.StoreType
 import org.prography.domain.model.store.BlogPostModel
+import org.prography.domain.model.store.StoreDetailModel
 import org.prography.utility.extensions.toSp
 
 @Composable
 fun HomeDetailScreen(
-    navHostController: NavHostController = rememberNavController(),
     homeDetailViewModel: HomeDetailViewModel = hiltViewModel(),
     storeId: Int,
+    onBack: () -> Unit
 ) {
     LaunchedEffect(storeId) {
         homeDetailViewModel.fetchDetailStore(storeId)
         homeDetailViewModel.fetchStoreBlogInfos(storeId)
     }
 
-    val storeDetailState = homeDetailViewModel.state.collectAsStateWithLifecycle()
-    val storeDetailModel = storeDetailState.value.storeDetailModel
-    val storeBlogPosts = storeDetailState.value.blogPosts
-    val scrollState = rememberScrollState()
-
+    val storeDetailUiState = homeDetailViewModel.state.collectAsStateWithLifecycle().value
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(White)
     ) {
-        HomeDetailAppbar(title = stringResource(R.string.home_detail_app_bar)) {
-            navHostController.popBackStack()
-        }
+        HomeDetailContent(
+            storeDetailModel = storeDetailUiState.storeDetailModel,
+            storeBlogPosts = storeDetailUiState.blogPosts,
+            onBack = onBack
+        )
+    }
+}
 
-        Column(
+@Composable
+private fun HomeDetailContent(
+    storeDetailModel: StoreDetailModel,
+    storeBlogPosts: List<BlogPostModel>,
+    onBack: () -> Unit
+) {
+    val scrollState = rememberScrollState()
+    CakkAppbarWithBack(
+        title = stringResource(R.string.home_detail_app_bar),
+        onClick = onBack
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        HomeDetailHeader(
+            modifier = Modifier.fillMaxWidth(),
+            storeName = storeDetailModel.name,
+            storeLocation = storeDetailModel.location,
+            storeLink = storeDetailModel.link,
+            storeThumbnail = storeDetailModel.thumbnail
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.img_default_cakeshop),
+            contentDescription = null,
             modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(scrollState)
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.img_default_cakeshop),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(360 / 176f)
-            )
+                .fillMaxWidth()
+                .aspectRatio(360 / 176f)
+        )
 
-            Text(
-                text = storeDetailModel.name,
-                modifier = Modifier
-                    .padding(top = 40.dp)
-                    .align(Alignment.CenterHorizontally),
-                color = Raisin_Black,
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.dp.toSp(),
-                fontFamily = pretendard,
-                letterSpacing = (-0.03).em
-            )
+        Text(
+            text = storeDetailModel.name,
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .align(Alignment.CenterHorizontally),
+            color = Raisin_Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.dp.toSp(),
+            fontFamily = pretendard,
+            letterSpacing = (-0.03).em
+        )
 
-            Text(
-                text = storeDetailModel.location,
-                modifier = Modifier
-                    .padding(top = 12.dp)
-                    .align(Alignment.CenterHorizontally),
-                color = Raisin_Black.copy(alpha = 0.8f),
-                fontSize = 16.dp.toSp(),
-                fontFamily = pretendard,
-                letterSpacing = (-0.03).em
-            )
+        Text(
+            text = storeDetailModel.location,
+            modifier = Modifier
+                .padding(top = 12.dp)
+                .align(Alignment.CenterHorizontally),
+            color = Raisin_Black.copy(alpha = 0.8f),
+            fontSize = 16.dp.toSp(),
+            fontFamily = pretendard,
+            letterSpacing = (-0.03).em
+        )
 
-            HomeDetailTab(
+        if (storeDetailModel.storeTypes.isNotEmpty()) {
+            HomeDetailKeywordRow(
                 modifier = Modifier
                     .padding(top = 32.dp)
-                    .fillMaxWidth()
-            )
-
-            if (storeDetailModel.storeTypes.isNotEmpty()) {
-                HomeDetailKeywordRow(
-                    modifier = Modifier
-                        .padding(top = 32.dp)
-                        .fillMaxWidth(),
-                    storeTypes = storeDetailModel.storeTypes.map { StoreType.valueOf(it) }
-                )
-            }
-
-            Spacer(
-                modifier = Modifier
-                    .padding(top = if (storeDetailModel.storeTypes.isNotEmpty()) 22.dp else 32.dp)
-                    .height(10.dp)
-                    .fillMaxWidth()
-                    .background(Platinum)
-            )
-
-            HomeDetailInfoRow(
-                modifier = Modifier
-                    .padding(top = 40.dp)
-                    .padding(horizontal = 16.dp)
                     .fillMaxWidth(),
-                location = storeDetailModel.location
-            )
-
-            Spacer(
-                modifier = Modifier
-                    .padding(top = 26.5.dp)
-                    .height(10.dp)
-                    .fillMaxWidth()
-                    .background(Platinum)
-            )
-
-            HomeDetailBlogRow(
-                modifier = Modifier
-                    .padding(top = 40.dp, bottom = 60.dp)
-                    .padding(horizontal = 16.dp),
-                blogPosts = storeBlogPosts
+                storeTypes = storeDetailModel.storeTypes.map { StoreType.valueOf(it) }
             )
         }
+
+        Spacer(
+            modifier = Modifier
+                .padding(top = if (storeDetailModel.storeTypes.isNotEmpty()) 22.dp else 32.dp)
+                .height(10.dp)
+                .fillMaxWidth()
+                .background(Platinum)
+        )
+
+        HomeDetailInfoRow(
+            modifier = Modifier
+                .padding(top = 40.dp)
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth(),
+            location = storeDetailModel.location
+        )
+
+        Spacer(
+            modifier = Modifier
+                .padding(top = 26.5.dp)
+                .height(10.dp)
+                .fillMaxWidth()
+                .background(Platinum)
+        )
+
+        HomeDetailBlogRow(
+            modifier = Modifier
+                .padding(top = 40.dp, bottom = 60.dp)
+                .padding(horizontal = 16.dp),
+            blogPosts = storeBlogPosts
+        )
+    }
+}
+
+@Composable
+private fun HomeDetailHeader(
+    modifier: Modifier = Modifier,
+    storeName: String,
+    storeLocation: String,
+    storeLink: String,
+    storeThumbnail: String? = null
+) {
+    Column(
+        modifier = modifier
+            .background(Palatinate_Blue.copy(alpha = 0.1f))
+            .padding(top = 30.dp, bottom = 24.dp)
+    ) {
+        HomeDetailHeaderInfo(
+            modifier = Modifier.fillMaxWidth(),
+            storeName = storeName,
+            storeLocation = storeLocation,
+            storeThumbnail = storeThumbnail
+        )
+
+        HomeDetailHeaderTab(
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .fillMaxWidth(),
+            storeLink = storeLink
+        )
+    }
+}
+
+@Composable
+private fun HomeDetailHeaderInfo(
+    modifier: Modifier = Modifier,
+    storeName: String,
+    storeLocation: String,
+    storeThumbnail: String? = null
+) {
+    Row(
+        modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        AsyncImage(
+            model = storeThumbnail,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = 20.dp)
+                .size(72.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop,
+            error = painterResource(R.drawable.img_default)
+        )
+
+        Column(modifier = Modifier.padding(start = 16.dp)) {
+            Text(
+                text = storeName,
+                color = Raisin_Black,
+                fontSize = 18.dp.toSp(),
+                fontWeight = FontWeight.Bold,
+                fontFamily = pretendard
+            )
+
+            Text(
+                text = storeLocation,
+                color = Raisin_Black.copy(alpha = 0.6f),
+                fontSize = 14.dp.toSp(),
+                fontWeight = FontWeight.Normal,
+                fontFamily = pretendard
+            )
+        }
+    }
+}
+
+@Composable
+private fun HomeDetailHeaderTab(
+    modifier: Modifier = Modifier,
+    storeLink: String
+) {
+    Row(modifier) {
+        HomeDetailHeaderTabItem(
+            iconRes = if (storeLink.isEmpty()) {
+                R.drawable.ic_kakao
+            } else if (storeLink.contains("instagram")) {
+                R.drawable.ic_instagram
+            } else {
+                R.drawable.ic_website
+            },
+            stringRes = if (storeLink.isEmpty()) {
+                R.string.home_detail_kakao_tab
+            } else if (storeLink.contains("instagram")) {
+                R.string.home_detail_instagram_tab
+            } else {
+                R.string.home_detail_website_tab
+            }
+        )
+
+        Spacer(
+            modifier = Modifier
+                .size(1.dp, 54.dp)
+                .background(Raisin_Black.copy(alpha = 0.05f))
+        )
+
+        HomeDetailHeaderTabItem(
+            iconRes = R.drawable.ic_heart,
+            stringRes = R.string.home_detail_bookmark
+        )
+
+        Spacer(
+            modifier = Modifier
+                .size(1.dp, 54.dp)
+                .background(Raisin_Black.copy(alpha = 0.05f))
+        )
+
+        HomeDetailHeaderTabItem(
+            iconRes = R.drawable.ic_navigation,
+            stringRes = R.string.home_detail_navigation
+        )
+
+        Spacer(
+            modifier = Modifier
+                .size(1.dp, 54.dp)
+                .background(Raisin_Black.copy(alpha = 0.05f))
+        )
+
+        HomeDetailHeaderTabItem(
+            iconRes = R.drawable.ic_share,
+            stringRes = R.string.home_detail_share
+        )
+    }
+}
+
+@Composable
+private fun RowScope.HomeDetailHeaderTabItem(
+    @DrawableRes iconRes: Int,
+    @StringRes stringRes: Int,
+    onClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier.weight(1f),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        IconButton(
+            onClick = onClick,
+            modifier = Modifier.size(40.dp)
+        ) {
+            Icon(
+                painter = painterResource(iconRes),
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+
+        Text(
+            text = stringResource(stringRes),
+            color = Raisin_Black.copy(alpha = 0.8f),
+            fontSize = 12.dp.toSp(),
+            fontWeight = FontWeight.Normal,
+            fontFamily = pretendard
+        )
     }
 }
 
@@ -441,7 +615,12 @@ private fun HomeDetailKeywordRow(
                 Text(
                     text = it.tag,
                     modifier = Modifier
-                        .background(it.toColor().copy(alpha = 0.2f), RoundedCornerShape(14.dp))
+                        .background(
+                            it
+                                .toColor()
+                                .copy(alpha = 0.2f),
+                            RoundedCornerShape(14.dp)
+                        )
                         .padding(vertical = 8.dp, horizontal = 12.dp),
                     color = it.toColor(),
                     fontSize = 12.dp.toSp(),
@@ -452,109 +631,5 @@ private fun HomeDetailKeywordRow(
                 )
             }
         }
-    }
-}
-
-@Composable
-private fun HomeDetailTab(
-    modifier: Modifier = Modifier,
-) {
-    Row(modifier = modifier.height(intrinsicSize = IntrinsicSize.Max)) {
-        HomeDetailTabItem(
-            drawableRes = R.drawable.ic_phone,
-            stringRes = R.string.home_detail_phone
-        )
-
-        Spacer(
-            modifier = Modifier
-                .width(1.dp)
-                .fillMaxHeight()
-                .background(Color.Gray)
-        )
-
-        HomeDetailTabItem(
-            drawableRes = R.drawable.ic_bookmark,
-            stringRes = R.string.home_detail_bookmark
-        )
-
-        Spacer(
-            modifier = Modifier
-                .width(1.dp)
-                .fillMaxHeight()
-                .background(Color.Gray)
-        )
-
-        HomeDetailTabItem(
-            drawableRes = R.drawable.ic_navigation,
-            stringRes = R.string.home_detail_navigation
-        )
-
-        Spacer(
-            modifier = Modifier
-                .width(1.dp)
-                .fillMaxHeight()
-                .background(Color.Gray)
-        )
-
-        HomeDetailTabItem(
-            drawableRes = R.drawable.ic_share,
-            stringRes = R.string.home_detail_share
-        )
-    }
-}
-
-@Composable
-private fun RowScope.HomeDetailTabItem(
-    @DrawableRes drawableRes: Int,
-    @StringRes stringRes: Int,
-) {
-    Column(
-        modifier = Modifier.weight(1f),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(drawableRes),
-            contentDescription = null,
-            modifier = Modifier.padding(top = 9.dp)
-        )
-
-        Text(
-            text = stringResource(stringRes),
-            modifier = Modifier.padding(top = 11.dp),
-            color = Raisin_Black.copy(alpha = 0.8f),
-            fontSize = 14.dp.toSp(),
-            fontFamily = pretendard,
-            letterSpacing = (-0.03).em
-        )
-    }
-}
-
-@Composable
-private fun HomeDetailAppbar(
-    title: String,
-    onBack: () -> Unit = {},
-) {
-    Box(modifier = Modifier.fillMaxWidth()) {
-        Image(
-            painter = painterResource(R.drawable.ic_left_arrow),
-            contentDescription = title,
-            modifier = Modifier
-                .align(Alignment.CenterStart)
-                .padding(start = 16.dp)
-                .padding(vertical = 9.dp)
-                .clickable(onClick = onBack)
-        )
-
-        Text(
-            text = title,
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(vertical = 9.dp),
-            color = Black,
-            fontSize = 17.dp.toSp(),
-            fontFamily = pretendard,
-            fontWeight = FontWeight.Bold,
-            letterSpacing = (-0.02).em
-        )
     }
 }
