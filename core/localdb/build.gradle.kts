7f25334d195e1864bda213cc13008dd839a34aa4
lee989898 <wnsrud1352@gmail.com>
2023-08-03 02:30:26 +0900

    [chore] #57 core localdb build.gradle datastore 의존성 추가 및 FILTER_PREFERENCES_NAME Build Config 추가

--- core/localdb/build.gradle.kts
+++ core/localdb/build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UnstableApiUsage")
 
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.android)
@@ -10,6 +12,10 @@ plugins {
 
 android {
     namespace = "org.prography.localdb"
+
+    defaultConfig {
+        buildConfigField("String", "FILTER_PREFERENCES_NAME", gradleLocalProperties(rootDir).getProperty("FILTER_PREFERENCES_NAME"))
+    }
 }
 
 dependencies {
@@ -17,6 +23,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.dagger.hilt.android)
+    implementation(libs.androidx.datastore)
     implementation(libs.bundles.androidx.room)
     kapt(libs.androidx.room.compiler)
     kapt(libs.dagger.hilt.compiler)
