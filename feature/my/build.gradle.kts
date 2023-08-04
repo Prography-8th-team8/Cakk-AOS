bad3ad61c5e2f02cb0944fc56dfcddc98190c0cc
ows3090 <ows3090@naver.com>
2023-08-04 13:36:24 +0900

    [feat] #58 feature my 모듈 추가

---
+++ feature/my/build.gradle.kts
@@ -0,0 +1,48 @@
+@file:Suppress("UnstableApiUsage")
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.secrets.gradle)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt)
+}
+
+android {
+    namespace = "org.prography.my"
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:utility"))
+    implementation(project(":core:base"))
+    implementation(project(":domain"))
+
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.compose.hilt.navigation)
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.material)
+    implementation(libs.timber)
+    implementation(libs.coil.compose)
+    implementation(libs.dagger.hilt.android)
+    implementation(libs.bundles.androidx.compose)
+    implementation(libs.naver.map)
+    implementation(libs.gms)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso)
+    kapt(libs.bundles.compiler)
+}
