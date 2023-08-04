a6cd100c206e14f2e1c4c17f86c303ef0873888e
ows3090 <ows3090@naver.com>
2023-08-04 17:28:34 +0900

    [feat] #58 utility -> designsystem -> utility Circular 순환으로 인한 common 모듈 추가

---
+++ common/build.gradle.kts
@@ -0,0 +1,31 @@
+@file:Suppress("UnstableApiUsage")
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "org.prography.common"
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":core:base"))
+    implementation(project(":core:designsystem"))
+    implementation(libs.timber)
+    implementation(libs.material)
+    implementation(libs.bundles.androidx.compose)
+}
