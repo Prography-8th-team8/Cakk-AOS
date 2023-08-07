7990d97697fe9b2d9ccd6bcaca7dd23d25919a6a
ows3090 <ows3090@naver.com>
2023-08-08 00:58:27 +0900

    [feat] #59 FeeScreen 앱 바 UI 개발

--- feature/feed/src/main/java/org/prography/feed/FeedScreen.kt
+++ feature/feed/src/main/java/org/prography/feed/FeedScreen.kt
@@ -1,8 +1,39 @@
 package org.prography.feed
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.prography.designsystem.component.CakkAppbar
+import org.prography.designsystem.R
 
 @Composable
 fun FeedScreen() {
+    Scaffold(
+        topBar = {
+            CakkAppbar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(42.dp),
+                title = stringResource(R.string.feed_app_bar)
+            )
+        }
+    ) { paddingValues ->
+        FeedContent(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        )
+    }
+}
 
+@Composable
+private fun FeedContent(
+    modifier: Modifier
+) {
 }
