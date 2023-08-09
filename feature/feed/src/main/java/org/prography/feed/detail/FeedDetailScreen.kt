8547f57824496a39ce8328e35cfdfc1219063bcb
ows3090 <ows3090@naver.com>
2023-08-10 01:25:47 +0900

    [feat] #81 피드 상세 화면 이미지 Pager 개발완료

--- feature/feed/src/main/java/org/prography/feed/detail/FeedDetailScreen.kt
+++ feature/feed/src/main/java/org/prography/feed/detail/FeedDetailScreen.kt
@@ -1,16 +1,22 @@
 package org.prography.feed.detail
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -19,6 +25,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -28,6 +35,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import kotlinx.coroutines.launch
 import org.prography.designsystem.R
 import org.prography.designsystem.component.CakkAppbarWithClose
 import org.prography.designsystem.ui.theme.Light_Deep_Pink
@@ -68,12 +77,15 @@ fun FeedDetailScreen(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun FeedDetailContent(
     modifier: Modifier = Modifier,
     storeLocation: String,
     storeImageUrls: List<String>
 ) {
+    val pagerState = rememberPagerState { storeImageUrls.size }
+    val scope = rememberCoroutineScope()
     Box(modifier) {
         Image(
             painter = painterResource(R.drawable.img_feed_detail),
@@ -98,6 +110,69 @@ private fun FeedDetailContent(
                     fontWeight = FontWeight.Bold,
                     fontFamily = pretendard
                 )
+            }
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .offset(y = (-44).dp)
+                .fillMaxWidth()
+        ) { index ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = storeImageUrls[index],
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.FillBounds
+                )
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            pagerState.scrollToPage(pagerState.currentPage - 1)
+                        }
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 16.dp)
+                        .size(38.dp)
+                        .background(White.copy(0.8f), CircleShape)
+                        .border(1.dp, Raisin_Black.copy(0.1f), CircleShape),
+                    enabled = pagerState.currentPage > 0
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_chevron_left),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp),
+                        tint = Raisin_Black.copy(0.6f)
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            pagerState.scrollToPage(pagerState.currentPage + 1)
+                        }
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 16.dp)
+                        .size(38.dp)
+                        .background(White.copy(0.8f), CircleShape)
+                        .border(1.dp, Raisin_Black.copy(0.1f), CircleShape),
+                    enabled = pagerState.currentPage < storeImageUrls.lastIndex
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.ic_chevron_right),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp),
+                        tint = Raisin_Black.copy(0.6f)
+                    )
+                }
             }
         }
         Row(
