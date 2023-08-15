09ff9f0f32bc55749b5583b6756a8010e25a8e30
lee989898 <wnsrud1352@gmail.com>
2023-08-15 15:52:26 +0900

    [chore] #61 BookmarkEntity에 @Serializable 어노테이션 추가

--- core/localdb/src/main/java/org/prography/localdb/entity/BookmarkEntity.kt
+++ core/localdb/src/main/java/org/prography/localdb/entity/BookmarkEntity.kt
@@ -2,8 +2,10 @@ package org.prography.localdb.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.serialization.Serializable
 import org.prography.domain.model.store.BookmarkModel
 
+@Serializable
 @Entity(tableName = "cake_store_bookmark_table")
 data class BookmarkEntity(
     @PrimaryKey
