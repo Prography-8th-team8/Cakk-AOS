28690087ca800e26d3ceabcfd7314b94697e5878
lee989898 <wnsrud1352@gmail.com>
2023-08-15 20:04:17 +0900

    [feat] #61 FeedScreen에서 북마크 기능 구현

--- feature/feed/src/main/java/org/prography/feed/detail/FeedDetailViewModel.kt
+++ feature/feed/src/main/java/org/prography/feed/detail/FeedDetailViewModel.kt
@@ -2,10 +2,14 @@ package org.prography.feed.detail
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 import org.prography.base.BaseViewModel
+import org.prography.domain.model.store.StoreModel
 import org.prography.domain.repository.StoreRepository
 import javax.inject.Inject
 
@@ -18,13 +22,58 @@ class FeedDetailViewModel @Inject constructor(
     override fun reduceState(currentState: FeedDetailState, action: FeedDetailAction): FeedDetailState =
         when (action) {
             FeedDetailAction.Loading -> currentState
-            is FeedDetailAction.LoadDetailInfo -> currentState.copy(action.storeDetailModel)
+            is FeedDetailAction.LoadDetailInfo -> currentState.copy(storeDetailModel = action.storeDetailModel)
+            is FeedDetailAction.LoadBookmarkedCakeShop -> {
+                currentState.copy(
+                    storeDetailModel = if (currentState.storeDetailModel.id == action.id) {
+                        currentState.storeDetailModel.copy(bookmarked = true)
+                    } else {
+                        currentState.storeDetailModel
+                    }
+                )
+            }
+            is FeedDetailAction.BookmarkCakeShop -> {
+                currentState.copy(
+                    storeDetailModel = if (currentState.storeDetailModel.id == action.id) {
+                        currentState.storeDetailModel.copy(bookmarked = true)
+                    } else {
+                        currentState.storeDetailModel
+                    }
+                )
+            }
+            is FeedDetailAction.UnBookmarkCakeShop -> {
+                currentState.copy(
+                    storeDetailModel = if (currentState.storeDetailModel.id == action.id) {
+                        currentState.storeDetailModel.copy(bookmarked = false)
+                    } else {
+                        currentState.storeDetailModel
+                    }
+                )
+            }
         }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     fun fetchStoreDetailInfo(storeId: Int) {
         storeRepository.fetchDetailStore(storeId)
             .onStart { sendAction(FeedDetailAction.Loading) }
             .onEach { sendAction(FeedDetailAction.LoadDetailInfo(it)) }
+            .flatMapMerge { storeRepository.fetchBookmarkedCakeShop(it.id) }
+            .onStart { sendAction(FeedDetailAction.Loading) }
+            .onEach { it?.id?.let { id -> sendAction(FeedDetailAction.LoadBookmarkedCakeShop(id)) } }
             .launchIn(viewModelScope)
+    }
+
+    fun bookmarkCakeShop(bookmarkModel: StoreModel) {
+        viewModelScope.launch {
+            storeRepository.bookmarkStore(bookmarkModel = bookmarkModel)
+            sendAction(FeedDetailAction.BookmarkCakeShop(bookmarkModel.id))
+        }
+    }
+
+    fun unBookmarkCakeShop(id: Int) {
+        viewModelScope.launch {
+            storeRepository.unBookmarkStore(id = id)
+            sendAction(FeedDetailAction.UnBookmarkCakeShop(id))
+        }
     }
 }
